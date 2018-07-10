49ab3e5c04d1df62afd898c75dc3d44e07a72147
cj <cjrh1films@gmail.com>
2018-07-10 13:31:48 -0700

    add refs on NewTapForm and console log user input

--- components/NewTapForm.jsx
+++ components/NewTapForm.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 
 function NewTapForm() {
+  let _name = null;
+  let _brand = null;
+  let _price = null;
+
+  function handleNewTapFormSubmission(event) {
+    event.preventDefault();
+    console.log(_name.value);
+    console.log(_brand.value);
+    console.log(_price.value);
+    _name.value = '';
+    _brand.value = '';
+    _price.value = '';
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewTapFormSubmission}>
         <input
           type='text'
           id='name'
-          placeholder='Name' />
+          placeholder='Name'
+          ref={(input) => {_name = input;}}/>
         <input
           type='text'
           id='brand'
-          placeholder='Brand' />
+          placeholder='Brand'
+          ref={(input) => {_brand = input;}}/>
         <textarea
           id='price'
-          placeholder='Price' />
+          placeholder='Price'
+          ref={(input) => {_price = input;}}/>
         <button type='submit'>Create</button>
       </form>
     </div>
