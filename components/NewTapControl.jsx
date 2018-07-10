cb0d99aad70de55b52f6ee8371ac33d6f48fe88e
cj <cjrh1films@gmail.com>
2018-07-10 12:52:59 -0700

    add NewTapControl.jsx componenet, along with handleTroubleshooting Confirmation lifting method

---
+++ components/NewTapControl.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTapForm from './NewTapForm';
+import PropTypes from 'prop-types';
+
+class NewTapForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+  }
+
+  handleTroubleshootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
+  }
+
+  render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewTapForm onNewTapCreation={this.props.onNewTapCreation}/>;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTrounleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
+    }
+    return (
+      <div>
+        {currentlyVisibleContent}
+      </div>
+    );
+  }
+}
+
+NewTapControl.propTypes = {
+  onNewTapCreation: PropTypes.func
+};
+
+export default NewTapControl;
